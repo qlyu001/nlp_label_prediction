52a1b5d52353dc465b56edbc4838d2940a1c3184
qlyu001 <qlyu001@ucr.edu>
2020-05-13 15:11:10 -0700

    update

---
+++ nlp_helper/install.sh
@@ -0,0 +1,7 @@
+fileid="1swWtUjFAh66GuYWM-ZJMsFYlmvgE6U2j"
+filename="roberta_few_labels.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" 
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+unzip roberta_few_labels.zip
+rm roberta_few_labels.zip
