b533b6079720811982078d1f451ac49984492b58
qlyu001 <qlyu001@ucr.edu>
2020-05-13 16:52:13 -0700

    update

--- nlp_helper/notation.py
+++ nlp_helper/notation.py
@@ -1,40 +1,59 @@
 
-from pprint import pprint
 import numpy as np
 from transformers import BertTokenizer, BertConfig, RobertaTokenizer
 from transformers import BertForTokenClassification, RobertaForTokenClassification
 import torch
+import pickle
 
 def sentenceLabel(sentence):
+    f = open('./model_save/tags_vals.pckl', 'rb')
+    tags_vals = pickle.load(f)
+    f = open('./model_save/tag2idx.pckl', 'rb')
+    tag2idx = pickle.load(f)
     device = torch.device("cpu")
     output_dir = './roberta_few_labels/'
+    idx2tag = dict((v,k) for k, v in tag2idx.items())
     tokenizer = RobertaTokenizer.from_pretrained(output_dir)
     model = RobertaForTokenClassification.from_pretrained(output_dir)
     model.to(device)
-    text = 'O, B-Diagnostic_procedure, I-Diagnostic_procedure,B-Biological_structure, I-Biological_structure, B-Sign_symptom, I-Sign_symptom, B-Detailed_description, I-Detailed_description, B-Lab_value, I-Lab_value, B-Date, I-Date, B-Age, I-Age, B-Clinical_event, I-Clinical_event, B-Date, I-Date, B-Disease_disorder, I-Disease_disorder, B-Nonbiological_location, I-Nonbiological_location, B-Severity, I-Severity, B-Sex, B-Therapeutic_procedure, I-Therapeutic_procedure'
-    tag_values =  text.split(',')
+
+    # predict
+    all_tokens = []
+    all_entities = []
+
     tokenized_sentence = tokenizer.encode(sentence)
     input_ids = torch.tensor([tokenized_sentence]).cuda()
 
+    input_ids = torch.tensor([tokenized_sentence]).to(device)
+
+    predictions = []
     with torch.no_grad():
         output = model(input_ids)
-        label_indices = np.argmax(output[0].to('cpu').numpy(), axis=2)
-        # predict
-        # join bpe split tokens
-        tokens = tokenizer.convert_ids_to_tokens(input_ids.to('cpu').numpy()[0])
-        new_tokens, new_labels = [], []
-        for token, label_idx in zip(tokens, label_indices[0]):
-            if token.startswith("##"):
-                new_tokens[-1] = new_tokens[-1] + token[2:]
-            else:
-                new_labels.append(tag_values[label_idx])
-                new_tokens.append(token)
-    pprint(new_labels)
-    pprint(new_tokens)
-    return new_labels,new_tokens
+        output = output[0].detach().cpu().numpy()
+        predictions.extend([list(p) for p in np.argmax(output, axis=2)])
+
+    tags_predictions = []
+    for x in predictions[0]:
+        tags_predictions.append(idx2tag[int(x)])
+
+    tokens = []
+    count = 0
+
+    ### get tokens from ids
+    for x in tokenizer.convert_ids_to_tokens(tokenized_sentence):
+        if count == 1:
+            tokens.append(x)
+        else:
+            tokens.append(x[1:])
+        count+=1
+
+    all_entities.append(tags_predictions[1:-1])
+    all_tokens.append(tokens[1:-1])
 
 
+    print(all_tokens)
+    print(all_entities)
 
+    return all_tokens,all_entities
 if __name__ == '__main__':
     sentenceLabel("a boy has fever and cold ")
-
