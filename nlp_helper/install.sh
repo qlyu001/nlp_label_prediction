9614cb0177f36ac5d3a0245e7fdf83d2d95fa3e6
qlyu001 <qlyu001@ucr.edu>
2020-05-13 15:45:10 -0700

    updates

--- nlp_helper/install.sh
+++ nlp_helper/install.sh
@@ -1,8 +1,16 @@
 python3 -m pip install -r requirements.txt
-fileid="1swWtUjFAh66GuYWM-ZJMsFYlmvgE6U2j"
-filename="roberta_few_labels.zip"
+fileid="16Neexi-QyX-WettcN6Oc6mNNuK4NlsIr"
+filename="roberta.zip"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+
+fileid="1TQpXv7M22A4wHro7GWX3bzFqKEy46ypG"
+filename="model_save.zip"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" 
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 
-unzip roberta_few_labels.zip
-rm roberta_few_labels.zip
+unzip model_save
+rm model_save.zip
+
+unzip roberta
+rm roberta.zip
