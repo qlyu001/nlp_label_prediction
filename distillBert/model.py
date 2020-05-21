3eda2b45e7d0f8ecddc1ad4f4060dbfb62951a1a
qlyu001 <qlyu001@ucr.edu>
2020-05-20 17:42:13 -0700

    add new model

---
+++ distillBert/model.py
@@ -0,0 +1,310 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm, trange
+import time
+start = time.time()
+
+data = pd.read_csv("few_labels_train_valid.csv", encoding="latin1").fillna(method="ffill")
+print(data.tail(10))
+
+
+class SentenceGetter(object):
+    
+    def __init__(self, data):
+        self.n_sent = 1
+        self.data = data
+        self.empty = False
+        agg_func = lambda s: [(w, p, t) for w, p, t in zip(s["Word"].values.tolist(),
+                                                           s["POS"].values.tolist(),
+                                                           s["Tag"].values.tolist())]
+        self.grouped = self.data.groupby("Sentence #").apply(agg_func)
+        self.sentences = [s for s in self.grouped]
+    
+    def get_next(self):
+        try:
+            s = self.grouped["Sentence: {}".format(self.n_sent)]
+            self.n_sent += 1
+            return s
+        except:
+            return None
+
+getter = SentenceGetter(data)
+
+sentences = [" ".join([s[0] for s in sent]) for sent in getter.sentences]
+print(sentences[0])
+
+labels = [[s[2] for s in sent] for sent in getter.sentences]
+print(labels[0])
+
+tags_vals = list(set(data["Tag"].values))
+tag2idx = {t: i for i, t in enumerate(tags_vals)}
+print(tags_vals)
+print(tag2idx)
+import pickle
+
+f = open('tags_vals.pckl', 'wb')
+pickle.dump(tags_vals, f)
+f.close()
+
+f = open('tag2idx.pckl', 'wb')
+pickle.dump(tag2idx, f)
+f.close()
+
+
+
+import torch
+from torch.optim import Adam
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from transformers import BertTokenizer, BertConfig, RobertaTokenizer
+ffrom transformers import DistilBertTokenizer, DistilBertModel
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Use plot styling from seaborn.
+def plotG( loss_values , validation_loss_values):
+    sns.set(style='darkgrid')
+    # Increase the plot size and font size.
+    sns.set(font_scale=1.5)
+    plt.rcParams["figure.figsize"] = (12,6)
+
+    # Plot the learning curve.
+    plt.plot(loss_values, 'b-o', label="training loss")
+    plt.plot(validation_loss_values, 'r-o', label="validation loss")
+
+        # Label the plot.
+    plt.title("Learning curve")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+
+    plt.show()
+
+MAX_LEN = 40
+bs = 12
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
+
+print(torch.cuda.get_device_name(0))
+
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-cased',do_lower_case=True)
+tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+print(tokenized_texts[0])
+
+input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts],
+                          maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+tags = pad_sequences([[tag2idx.get(l) for l in lab] for lab in labels],
+                     maxlen=MAX_LEN, value=tag2idx["O"], padding="post",
+                     dtype="long", truncating="post")
+
+attention_masks = [[float(i>0) for i in ii] for ii in input_ids]
+
+tr_inputs, val_inputs, tr_tags, val_tags = train_test_split(input_ids, tags,
+                                                            random_state=2018, test_size=0.1)
+tr_masks, val_masks, _, _ = train_test_split(attention_masks, input_ids,
+                                             random_state=2018, test_size=0.1)
+
+
+
+#torch tensors
+tr_inputs = torch.tensor(tr_inputs)
+val_inputs = torch.tensor(val_inputs)
+tr_tags = torch.tensor(tr_tags)
+val_tags = torch.tensor(val_tags)
+tr_masks = torch.tensor(tr_masks)
+val_masks = torch.tensor(val_masks)
+
+
+
+
+train_data = TensorDataset(tr_inputs, tr_masks, tr_tags)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=bs)
+
+valid_data = TensorDataset(val_inputs, val_masks, val_tags)
+valid_sampler = SequentialSampler(valid_data)
+valid_dataloader = DataLoader(valid_data, sampler=valid_sampler, batch_size=bs)
+
+
+model = DistilBertModel.from_pretrained('distilbert-base-cased',num_labels=len(tag2idx))
+model.cuda()
+
+
+FULL_FINETUNING = True
+if FULL_FINETUNING:
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'gamma', 'beta']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay_rate': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+         'weight_decay_rate': 0.0}
+    ]
+else:
+    param_optimizer = list(model.classifier.named_parameters())
+    optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
+optimizer = Adam(optimizer_grouped_parameters, lr=3e-5)
+
+from seqeval.metrics import f1_score
+
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=2).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+
+epochs = 10
+max_grad_norm = 3.0
+
+loss_values, validation_loss_values, f1_scores = [], [], []
+for _ in trange(epochs, desc="Epoch"):
+    # TRAIN loop
+    model.train()
+    tr_loss = 0
+    nb_tr_examples, nb_tr_steps = 0, 0
+    for step, batch in enumerate(train_dataloader):
+        # add batch to gpu
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        # forward pass
+        b_input_ids=torch.tensor(b_input_ids).to(torch.int64)
+        b_labels = b_labels.long()
+
+        loss, x = model(b_input_ids, token_type_ids=None,
+                     attention_mask=b_input_mask, labels=b_labels)
+        # backward pass
+        loss.backward()
+        # track train loss
+        tr_loss += loss.item()
+        nb_tr_examples += b_input_ids.size(0)
+        nb_tr_steps += 1
+        # gradient clipping
+        torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=max_grad_norm)
+        # update parameters
+        optimizer.step()
+        model.zero_grad()
+    # print train loss per epoch
+    print("Train loss: {}".format(tr_loss/nb_tr_steps))
+    loss_values.append(tr_loss/nb_tr_steps)
+
+    # VALIDATION on validation set
+    model.eval()
+    eval_loss, eval_accuracy = 0, 0
+    nb_eval_steps, nb_eval_examples = 0, 0
+    predictions , true_labels = [], []
+    for batch in valid_dataloader:
+        batch = tuple(t.to(device) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+        b_input_ids=torch.tensor(b_input_ids).to(torch.int64)
+        b_labels = b_labels.long()
+        with torch.no_grad():
+            tmp_eval_loss, x = model(b_input_ids, token_type_ids=None,
+                                  attention_mask=b_input_mask, labels=b_labels)
+            logits = model(b_input_ids, token_type_ids=None,
+                           attention_mask=b_input_mask)
+        logits = logits[0].detach().cpu().numpy()
+        label_ids = b_labels.to('cpu').numpy()
+        predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
+        true_labels.append(label_ids)
+        
+        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+        
+        eval_loss += tmp_eval_loss.mean().item()
+        eval_accuracy += tmp_eval_accuracy
+        
+        nb_eval_examples += b_input_ids.size(0)
+        nb_eval_steps += 1
+    eval_loss = eval_loss/nb_eval_steps
+    validation_loss_values.append(eval_loss)
+    print("Validation loss: {}".format(eval_loss))
+    print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+    pred_tags = [tags_vals[p_i] for p in predictions for p_i in p]
+    valid_tags = [tags_vals[l_ii] for l in true_labels for l_i in l for l_ii in l_i]
+    print("F1-Score: {}".format(f1_score(pred_tags, valid_tags, average="macro")))
+    #plotG(loss_values,validation_loss_values)
+
+plotG(loss_values,validation_loss_values)
+
+
+
+model.eval()
+predictions = []
+true_labels = []
+eval_loss, eval_accuracy = 0, 0
+nb_eval_steps, nb_eval_examples = 0, 0
+for batch in valid_dataloader:
+    batch = tuple(t.to(device) for t in batch)
+    b_input_ids, b_input_mask, b_labels = batch
+    b_input_ids=torch.tensor(b_input_ids).to(torch.int64)
+    #print("b_input_ids")
+    #print(b_input_ids)
+    #print(b_input_ids.shape)
+    #print(b_input_mask)
+    b_labels = b_labels.long()
+
+    with torch.no_grad():
+        tmp_eval_loss, x = model(b_input_ids, token_type_ids=None,
+                              attention_mask=b_input_mask, labels=b_labels)
+        logits = model(b_input_ids, token_type_ids=None,
+                       attention_mask=b_input_mask)
+        
+    logits = logits[0].detach().cpu().numpy()
+    predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
+    label_ids = b_labels.to('cpu').numpy()
+    true_labels.append(label_ids)
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+
+    eval_loss += tmp_eval_loss.mean().item()
+    eval_accuracy += tmp_eval_accuracy
+
+    nb_eval_examples += b_input_ids.size(0)
+    nb_eval_steps += 1
+
+pred_tags = [[tags_vals[p_i] for p_i in p] for p in predictions]
+
+valid_tags = [[tags_vals[l_ii] for l_ii in l_i] for l in true_labels for l_i in l ]
+
+end = time.time()
+hours, rem = divmod(end-start, 3600)
+minutes, seconds = divmod(rem, 60)
+print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+
+print("--- %s seconds ---" % (time.time() - start))
+print("Validation loss: {}".format(eval_loss/nb_eval_steps))
+print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+print("Validation F1-Score: {}".format(f1_score(pred_tags, valid_tags)))
+
+
+
+from sklearn.metrics import classification_report, accuracy_score
+
+flat_pred = [item for sublist in pred_tags for item in sublist]
+flat_valid = [item for sublist in valid_tags for item in sublist]
+
+print(accuracy_score(flat_valid, flat_pred))
+print(classification_report(flat_valid, flat_pred))
+
+
+import os
+
+# Saving best-practices: if you use defaults names for the model, you can reload it using from_pretrained()
+
+output_dir = './model_save/'
+
+# Create output directory if needed
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+print("Saving model to %s" % output_dir)
+
+# Save a trained model, configuration and tokenizer using `save_pretrained()`.
+# They can then be reloaded using `from_pretrained()`
+model_to_save = model.module if hasattr(model, 'module') else model  # Take care of distributed/parallel training
+model_to_save.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
+
