e888a414ebad64287059fcb4506b82ebb222c8f5
qlyu001 <qlyu001@ucr.edu>
2020-05-20 18:34:04 -0700

    updates

---
+++ distillBert/label.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm, trange
+import time
+from transformers import BertTokenizer, BertConfig
+from keras.preprocessing.sequence import pad_sequences
+import torch
+start = time.time()
+
+data = pd.read_csv("few_labels_train_valid.csv", encoding="latin1").fillna(method="ffill")
+print(data.tail(10))
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+n_gpu = torch.cuda.device_count()
+
+class SentenceGetter(object):
+    
+    def __init__(self, data):
+        self.n_sent = 1
+        self.data = data
+        self.empty = False
+        agg_func = lambda s: [(w, p, t) for w, p, t in zip(s["Word"].values.tolist(),
+                                                           s["POS"].values.tolist(),
+                                                           s["Tag"].values.tolist())]
+        self.grouped = self.data.groupby("Sentence #").apply(agg_func)
+        self.sentences = [s for s in self.grouped]
+    
+    def get_next(self):
+        try:
+            s = self.grouped["Sentence: {}".format(self.n_sent)]
+            self.n_sent += 1
+            return s
+        except:
+            return None
+
+getter = SentenceGetter(data)
+
+sentences = [" ".join([s[0] for s in sent]) for sent in getter.sentences]
+print(sentences[0])
+
+labels = [[s[2] for s in sent] for sent in getter.sentences]
+print(labels[0])
+
+tags_vals = list(set(data["Tag"].values))
+tag2idx = {t: i for i, t in enumerate(tags_vals)}
+print(tags_vals)
+print(tag2idx)
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased', do_lower_case=False)
+
+def tokenize_and_preserve_labels(sentence, text_labels):
+    tokenized_sentence = []
+    labels = []
+
+    for word, label in zip(sentence, text_labels):
+
+        # Tokenize the word and count # of subwords the word is broken into
+        tokenized_word = tokenizer.tokenize(word)
+        n_subwords = len(tokenized_word)
+
+        # Add the tokenized word to the final tokenized word list
+        tokenized_sentence.extend(tokenized_word)
+
+        # Add the same label to the new list of labels `n_subwords` times
+        labels.extend([label] * n_subwords)
+
+    return tokenized_sentence, labels
+    
+tokenized_texts_and_labels = [
+    tokenize_and_preserve_labels(sent, labs)
+    for sent, labs in zip(sentences, labels)
+]
+
+#tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts], maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+tags = pad_sequences([[tag2idx.get(l) for l in lab] for lab in labels], maxlen=MAX_LEN, value=tag2idx["O"], padding="post", dtype="long", truncating="post")
+print(tokenized_texts)
+print(tokenized_texts)
+
