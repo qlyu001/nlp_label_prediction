0aa80822d4030551d60a23dcfff01ecdeb52de63
qlyu001 <qlyu001@ucr.edu>
2020-05-24 12:03:00 -0700

    add flair script

---
+++ nlp_helper/flair_label.py
@@ -0,0 +1,55 @@
+from flair.data import Sentence
+from flair.models import SequenceTagger
+from nltk import word_tokenize
+def tokenize(s):
+    s = s.replace('-', ' - ')       # deal with special case 17-year-old
+    return ' '.join(word_tokenize(s))
+def locationIndex(sentences):
+    start = 0
+    end = 0
+    location = []
+    for sentence in sentences:
+        for words in sentence:
+            for word in words:
+                end = start + len(word)
+                #print(word)
+                location.append([start,end])
+                start = end + 1
+    return location
+def sentenceLabel(sentence):
+    model = SequenceTagger.load_from_file('./best-model.pt')
+    sentence = [tokenize(q) for q in sentence.split('\\n')]
+    # predict
+    all_tokens = []
+    all_entities = []
+    for q in sentence:
+        sen = Sentence(q)
+        
+        model.predict(sen)
+        tokens = []
+        entity_types = []
+        for t in sen.tokens:
+            token = t.text
+            entity = t.tags['ner'].value
+            tokens.append(token)
+            entity_types.append(entity)
+        
+        all_tokens.append(tokens)
+        all_entities.append(entity_types)
+    #print(all_tokens)
+    #print(all_entities)
+    return all_tokens,all_entities
+    
+if __name__ == '__main__':
+    f = open("15939911.txt", "r")
+    sentences = []
+    tokens = []
+for x in f:
+    all_tokens,all_entities = sentenceLabel(x)
+    sentences.append(all_tokens)
+    tokens.append(all_entities)
+    
+print(sentences)
+print(tokens)
+location = locationIndex(sentences)
+#print(location)
